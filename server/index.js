2359d936fdb72c9cea5f5f9c35aaa80badd373aa
SomiyaMahmud <somiyamahmud15@gmail.com>
2024-12-17 06:16:45 +0600

    registration, login, logout backend

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const authRoutes = require("./routes/auth.js");
+const userRoutes = require("./routes/user.js");
+const cors = require("cors");
+const multer = require("multer");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+// Middlewares
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Adjust this if your frontend is hosted elsewhere
+  })
+);
+
+app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(8800, () => {
+  console.log("API working!");
+});
